200878b8d5292a788ead1b6e9928be783e6994ca
JRC-Capucho <joaorobertoc.capucho@gmail.com>
2023-06-22 08:19:25 -0300

    feat(disponibilizar): primeiro commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Função do AWS Lambda para buscar dados no DynamoDB
+
+## ☕ Como usar Função AWS Lambda
+
+Para usar a função, siga estas etapas:
+
+* Criar uma função no AWS Lambda.
+* Copiar e colocar o código.
+* Alterar o nome do da tabela para o nome da sua tabela.
+* Criar um rota no AWS API Gateway
+
+Como alternativa, consulte a documentação do AWS Lambda em [AWS Lambda](https://docs.aws.amazon.com/pt_br/lambda/latest/dg/services-apigateway.html).
---
+++ api.py
@@ -0,0 +1,35 @@
+import boto3
+import json
+from decimal import Decimal
+from boto3.dynamodb.conditions import Attr
+    
+def convert_decimal_json(data):
+    if isinstance(data, Decimal):
+        return str(data)
+    
+    
+def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb') # declarando que e dynamodb que irei usar
+    
+    table = dynamodb.Table('data-thing') # nome da tabela
+    
+        # filter do time de  0 ate o final
+        # me devolve uma o lista
+    response = table.scan(FilterExpression=Attr('time').gte(0))
+    
+    data =  response["Items"] # lista 
+    
+    datas = []
+    
+    for chunk in data:
+        temp = chunk['device_data']['Temperature']
+        humi = chunk['device_data']['Humidity']
+        res = {
+            "time": chunk['time'],
+            "id": chunk['id'],
+            "Humidity": humi,
+            "Temperature": temp
+            }
+        datas.append(res)
+    
+    return json.dumps(datas, default=convert_decimal_json)
